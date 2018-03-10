4ddc66693ea4517abaab75cd82600ec40aef7142
Carlos ' Arjona <eduardo.arjona@hotmail.com>
2018-03-09 21:25:26 -0300

    criando as classes

---
+++ src/SIGED.ApplicationCore/Entity/Matricula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIGED.ApplicationCore.Entity
+{
+   public class Matricula
+    {
+        public int MatriculaId { get; set; }
+        public int EstudanteId { get; set; }
+        public Estudante Estudante { get; set; }
+        public int CursoId { get; set; }
+        public Curso Curso { get; set; }
+        
+    }
+}
